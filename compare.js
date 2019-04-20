0a811e4ecf129a1491570f4fc688905271c4c96d
mjasim <mahmood.jasim.bd@gmail.com>
2019-04-19 22:43:52 -0400

    comments upgraded, comparison logic added

--- compare.js
+++ compare.js
@@ -2,11 +2,12 @@ var data = null;
 
 // populate all dropdowns
 function populate_dropdown() {
-    //criteria = get_comparison_criteria(data);
-    criteria = ["A", "B", "C", "D", "E",];
+    criteria = get_comparison_criteria(data);
+    //criteria = ["A", "B", "C", "D", "E",];
     for (var i = 0; i < criteria.length; i++) {
         $("#select-top").append('<option value="' + i + '">' + criteria[i] + '</option>');
     }
+    $("#select-top").append('<option value="' + i + '">' + "All" + '</option>');
 
     product_names = []
     for (var i in data) {
@@ -21,7 +22,6 @@ function populate_dropdown() {
     }
 }
 
-
 // read data and call initiator
 d3.json("alfred_data_camera.json", function (err, json) {
     data = JSON.parse(JSON.stringify(json))
@@ -29,12 +29,26 @@ d3.json("alfred_data_camera.json", function (err, json) {
     populate_dropdown();
 })
 
+
+// top dropdown mechanisms
+$('#select-top').change(function () {
+    var selectedText = $("#select-left").find("option:selected").text();
+    var compare_comments = get_comments(selectedText);
+    draw_comments("left", compare_comments)
+
+    var selectedText = $("#select-right").find("option:selected").text();
+    var compare_comments = get_comments(selectedText);
+    draw_comments("right", compare_comments)
+});
+
+// left dropdown mechanisms
 $('#select-left').change(function () {
     var selectedText = $(this).find("option:selected").text();
     var compare_comments = get_comments(selectedText);
     draw_comments("left", compare_comments)
 });
 
+// right dropdown mechanisms
 $('#select-right').change(function () {
     var selectedText = $(this).find("option:selected").text();
     var compare_comments = get_comments(selectedText);
@@ -44,28 +58,67 @@ $('#select-right').change(function () {
 // draw comments 
 function draw_comments(dropbar, compare_comments) {
 
+    var myNode = dropbar == "left" ? document.getElementById("compare_left_body_div") : document.getElementById("compare_right_body_div");
+    while (myNode.firstChild) {
+        myNode.removeChild(myNode.firstChild);
+    }
+
+    var selectedText = $("#select-top").find("option:selected").text();
+
     for (var i in compare_comments) {
         divComment = document.createElement("div")
         divComment.className = "compareCommentDiv"
-        divComment.id = dropbar == "left" ?  "compareCommentLeftDivId-" + i : "compareCommentRightDivId-" + i
-        
-        var divCommentHTML =
+        divComment.id = dropbar == "left" ? "compareCommentLeftDivId-" + i : "compareCommentRightDivId-" + i
 
-            "<div class=\"comment-body\"" + "\">" +
-            '<div style="float: left;padding-left: 2px;"><span class="sentiment_icon" id="span_id_senti_' + divComment.id + '">' +
-            "<i class=" + "></i>" + compare_comments[i].sentiment + '</span>' +
-            '<span class="vulgarity_icon" id="span_id_vul_' + divComment.id + '" >' +
-            "<i class=" + "></i>" + compare_comments[i].sentiment + '</span>' +
-            '<span class="star_icon" id="span_id_star_' + divComment.id + '" >' +
-            "<i class=" + "></i>" + compare_comments[i].star_rating + '</span>' +
-            //"<i class=" + "\"fas fa-plus-circle fa-lg\"" + "></i>" +
-            '</span></div>' + "\xa0\xa0" +
-            '<div style="float:left; padding-left: 5px"><p class="search_enable">' + compare_comments[i].review_body + "\xa0" +
-            "</p></div>";
+        var sent = "";
 
-        divComment.innerHTML = divCommentHTML;
-        var element = dropbar == "left" ? document.getElementById("compare_left_body_div") : document.getElementById("compare_right_body_div");
-        element.appendChild(divComment);
+        if (compare_comments[i].sentiment == "negative")
+            sent = "\"fas fa-arrow-down\"";
+        else if (compare_comments[i].sentiment == "positive")
+            sent = "\"fas fa-arrow-up\"";
+        else
+            sent = "\"fas fa-arrow-right\"";
+        if (selectedText == "" || selectedText == "All" || compare_comments[i].keywords.includes(selectedText)) {
+
+            // var divCommentHTML =
+
+            //     "<div class=\"comment-body\"" + "\">" +
+            //     '<div style="float: left;padding-left: 2px;"><span class="sentiment_icon" id="span_id_senti_' + divComment.id + '">' +
+            //     "<i class=" + sent + "></i>" + "\xa0\xa0" + '</span>' +
+            //     '<span class="vulgarity_icon" id="span_id_vul_' + divComment.id + '" >' +
+            //     "<i class=" + "></i>" + compare_comments[i].vulgarity + "\xa0\xa0" + '</span>' +
+            //     '<span class="star_icon" id="span_id_star_' + divComment.id + '" >' +
+            //     "<i class=" + "></i>" + compare_comments[i].star_rating + "\xa0\xa0" + '</span>' +
+            //     '</span></div>' + "\xa0\xa0" +
+            //     '<div style="float:left; padding-left: 5px"><p class="search_enable">' + compare_comments[i].review_body + "\xa0" +
+            //     "</p></div>"; 
+
+                var divCommentHTML =
+
+                "<div class=\"comment-body\"" + "\">" +
+                '<div style="float: left;padding-left: 2px;"><span class="sentiment_icon" id="span_id_senti_' + divComment.id + '">' +
+                "<i class=" + sent + "></i>" + "\xa0\xa0" + '</span>' +
+                '<span class="vulgarity_icon" id="span_id_vul_' + divComment.id + '" >' +
+                "<i class=" + "></i>" + compare_comments[i].vulgarity + "\xa0\xa0" + '</span>' +
+                '<span class="star_icon" id="span_id_star_' + divComment.id + '" >'; 
+
+                for(var j = 0; j < compare_comments[i].star_rating; j++){
+                    divCommentHTML += "<i class=\"fas fa-star\"" + "style=\"color:#FFC107\"" + "></i>" + "\xa0\xa0" + '</span>'; 
+                }
+
+                for(var j = 0; j < 5 - compare_comments[i].star_rating; j++){
+                    divCommentHTML += "<i class=\"far fa-star\"" + "></i>" + "\xa0\xa0" + '</span>'; 
+                }
+                
+                //"<i class=" + "\"fas fa-plus-circle fa-lg\"" + "></i>" +
+                divCommentHTML += '</span></div>' + "\xa0\xa0" +
+                '<div style="float:left; padding-left: 5px"><p class="search_enable">' + compare_comments[i].review_body + "\xa0" +
+                "</p></div>"; 
+
+            divComment.innerHTML = divCommentHTML;
+            var element = dropbar == "left" ? document.getElementById("compare_left_body_div") : document.getElementById("compare_right_body_div");
+            element.appendChild(divComment);
+        }
     }
 }
 
@@ -79,4 +132,29 @@ function get_comments(selectedText) {
         }
     }
     return compare_comments;
+}
+
+// get criteria for comparisons
+function get_comparison_criteria() {
+    all_keywords = []
+    for (var i in data) {
+        for (var j in data[i].keywords) {
+            all_keywords.push(data[i].keywords[j])
+        }
+    }
+
+    return (getFrequency(all_keywords, 5));
+}
+
+function getFrequency(all_keywords, cutOff) {
+    var frequencies = {}
+    for (i = 0; i < all_keywords.length; i++) {
+        keyword = all_keywords[i];
+        frequencies[keyword] = frequencies[keyword] || 0;
+        frequencies[keyword]++;
+    }
+
+    var keywords = Object.keys(frequencies);
+    x = keywords.sort(function (a, b) { return frequencies[b] - frequencies[a]; }).slice(0, cutOff).toString();
+    return x.split(",")
 }
