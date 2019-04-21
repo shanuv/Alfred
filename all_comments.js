a2e39cecd5d8ffe7657559a8026da55090a5eb5a
mjasim <mahmood.jasim.bd@gmail.com>
2019-04-21 17:45:14 -0400

    all comments added

--- all_comments.js
+++ all_comments.js
@@ -20,16 +20,16 @@ d3.json("alfred_data_camera_newkeywords.json", function (err, json) {
     data = JSON.parse(JSON.stringify(json))
     //console.log(data)
     populate_dropdown();
-    var selectedText = "B00XHRW9TI"; //product_id_selected
+    var selectedText = product_id_selected
     var all_comments = get_all_comments(selectedText);
     console.log(all_comments)
     draw_all_comments(all_comments)
 })
 
-
 // top dropdown mechanisms
 $('#select-top-all').change(function () {
-    var selectedText = "B00XHRW9TI"; //product_id_selected
+    //var selectedText = $(this).find("option:selected").text();
+    var selectedText = product_id_selected
     var all_comments = get_all_comments(selectedText);
     draw_all_comments(all_comments)
 });
@@ -43,12 +43,11 @@ function draw_all_comments(all_comments) {
     }
 
     var selectedText = $("#select-top-all").find("option:selected").text();
-    console.log(selectedText)
 
     for (var i in all_comments) {
         divComment = document.createElement("div")
         divComment.className = "allCommentDiv"
-        divComment.id = "allCommentLeftDivId-" + i 
+        divComment.id = "allCommentLeftDivId-" + i;
 
         var sent = "";
 
@@ -75,10 +74,10 @@ function draw_all_comments(all_comments) {
 
             var divCommentHTML =
 
-                "<div class=\"comment-body\"" + "\">" +
-                '<div style="float: left;padding-left: 2px;"><span class="sentiment_icon" id="span_id_senti_' + divComment.id + '">' +
+                "<div class=\"all-comment-body\"" + "\">" +
+                '<div style="float: left;padding-left: 2px;"><span class="all_sentiment_icon" id="all_span_id_senti_' + divComment.id + '">' +
                 "<i class=" + sent + "></i>" + "\xa0\xa0" + '</span>' +
-                '<span class="vulgarity_icon" id="span_id_vul_' + divComment.id + '" >';
+                '<span class="all_vulgarity_icon" id="all_span_id_vul_' + divComment.id + '" >';
 
             if (all_comments[i].vulgarity > 0 && all_comments[i].vulgarity <= 0.25)
                 divCommentHTML += "<i class=\"fas fa-circle fa-lg\"" + "style=\"color:#8CC9CD\"" + "></i>" + "\xa0\xa0" + '</span>';
@@ -89,7 +88,7 @@ function draw_all_comments(all_comments) {
             else if (all_comments[i].vulgarity > 0.75 && all_comments[i].vulgarity <= 1.0)
                 divCommentHTML += "<i class=\"fas fa-circle fa-lg\"" + "style=\"color:#32759B\"" + "></i>" + "\xa0\xa0" + '</span>';
 
-            divCommentHTML += '<span class="star_icon" id="span_id_star_' + divComment.id + '" >';
+            divCommentHTML += '<span class="all_star_icon" id="all_span_id_star_' + divComment.id + '" >';
 
             for (var j = 0; j < all_comments[i].star_rating; j++) {
                 divCommentHTML += "<i class=\"fas fa-star fa-lg\"" + "style=\"color:#FFC107\"" + "></i>" + "\xa0\xa0" + '</span>';
